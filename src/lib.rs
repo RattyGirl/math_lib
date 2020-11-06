dcf06efe278533fe5b077e908bdb1021e898e9b1
Rat Girl <chloeholmes8192@gmail.com>
2020-11-06 11:55:57 +1100

    Inserted Copyright Info

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,21 @@
+/*
+  Math_Lib is a Rust Library that contains many functions to be used
+  with the intention of calculations.
+  Copyright (C) 2020 Chloe Holmes
 
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or any
+  later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 /*
 Static functions
 */
